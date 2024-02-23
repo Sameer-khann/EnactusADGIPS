8f15de579009093c809505c12519c1857ea34228
Sameer Khan <sameerkhann241@gmail.com>
2024-02-23 16:25:51 +0530

    Initialized the Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
+import LandingPage from '../src/Pages/LandingPage.js';
+import AboutPage from '../src/Pages/AboutPage.js';
+import EventPage from '../src/Pages/EventPage.js';
+import TeamPage from '../src/Pages/TeamPage.js';
+import ProjectPage from '../src/Pages/ProjectPage.js';
+import Navbar from '../src/Components/Navbar.js';
+import Footer from '../src/Components/Footer.js';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+        <ScrollToTop/>
+        <Routes>
+          <Route path="/" element={<LandingPage/>} />
+          <Route path="/AboutPage" element={<AboutPage/>} />
+          <Route path="/EventPage" element={<EventPage />} />
+          <Route path="/TeamPage" element={<TeamPage />} />
+          <Route path="/ProjectPage" element={<ProjectPage/>} />
+          <Route path="/Attendance" element={<Attendance/>}/>
+        </Routes>
+        <Footer/>
+      </Router>
+    </>
   );
 }
 
