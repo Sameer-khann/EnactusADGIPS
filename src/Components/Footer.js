a59e6e79ecf545b287234fba413f6aa393d6e2b6
Ankur Sharma <ankursh2003@gmail.com>
2024-02-29 23:38:07 +0530

    adds footer and navbar

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import './Footer.css';
+import React from "react"
+import "./Footer.css"
 
 export default function Footer() {
     return (
         <div>
-            <footer className="footer">
-                <p>&copy; 2024 Your Company</p>
-            </footer>
+            <div class="footer">
+                <div class="first">
+                    <div class="heading">Connect With Us</div>
+                    <div class="socials">
+                        <a
+                            href="https://www.linkedin.com/company/enactus-adgitm"
+                            class="firstlogo"
+                        ></a>
+                        <a
+                            href="https://www.instagram.com/enactus.adgitm/"
+                            class="secondlogo"
+                        ></a>
+                        <a
+                            href="https://twitter.com/adgitmenactus"
+                            class="thirdlogo"
+                        ></a>
+                    </div>
+                    <div class="email">
+                        <div class="c1">enactusadgitm@gmail.com</div>
+                    </div>
+                    <div class="address">
+                        <div class="c2">
+                            FC-26, Panduk Shila Marg, Zero Pusta Rd, Shastri
+                            Park, Shahdara, New Delhi, Delhi-110053
+                        </div>
+                    </div>
+                </div>
+                <div class="second">
+                    <div class="main-logo1"></div>
+                    <div class="main-logo2"></div>
+                </div>
+            </div>
+            <div class="copyright">
+                <div class="l1">Enactus ADGITM © 2020</div>
+                <div class="l2">All Rights Reserved</div>
+            </div>
         </div>
     )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,35 @@
-import React from 'react'
-import './Navbar.css';
+import React from "react"
+import "./Navbar.css"
 
 export default function Navbar() {
     return (
         <>
-            <nav className="navbar">
-                <ul className="nav-links">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#events">Events</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                </ul>
-            </nav>
+            <div class="navbar">
+                <div class="nav-logoborder">
+                    <div class="logo1"></div>
+                    <div class="logo2"></div>
+                </div>
+                <div class="content">
+                    <div class="home">
+                        <a href="https://enactusadgitm.in/">Home</a>
+                    </div>
+                    <div class="about">
+                        <a href="">About</a>
+                    </div>
+                    <div class="store">
+                        <a href="https://darpan.enactusadgitm.in/">Store</a>
+                    </div>
+                    <div class="projects">
+                        <a href="https://enactusadgitm.in/projects">Projects</a>
+                    </div>
+                    <div class="Team">
+                        <a href="">Team</a>
+                    </div>
+                    <div class="events">
+                        <a href="">Events</a>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
