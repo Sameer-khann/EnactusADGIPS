e2aa3714a7044ed73be7406eb4349ee5a461c2fe
MOHDAZAM-08 <112116944+MOHDAZAM-08@users.noreply.github.com>
2024-03-05 23:46:18 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -33,20 +33,4 @@ export default function Navbar() {
         </>
     )
 }
-import React from 'react'
-import './Navbar.css';
 
-export default function Navbar() {
-    return (
-        <>
-            <nav className="navbar">
-                <ul className="nav-links">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#events">Events</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                </ul>
-            </nav>
-        </>
-    )
-}
