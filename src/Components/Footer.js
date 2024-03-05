6c18b8681a09b8be05e25eaf351130a121f1bbda
MOHDAZAM-08 <112116944+MOHDAZAM-08@users.noreply.github.com>
2024-03-05 23:46:34 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -43,15 +43,4 @@ export default function Footer() {
         </div>
     )
 }
-import React from 'react'
-import './Footer.css';
 
-export default function Footer() {
-    return (
-        <div>
-            <footer className="footer">
-                <p>&copy; 2024 Your Company</p>
-            </footer>
-        </div>
-    )
-}
