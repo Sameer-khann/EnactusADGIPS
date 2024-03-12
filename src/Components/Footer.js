8f88e60293e2b74acca892ca6e0d3f8ef09594fc
Ankur Sharma <ankursh2003@gmail.com>
2024-03-12 22:32:16 +0530

    changes in footer done on 12/3/24

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,65 +5,58 @@ export default function Footer() {
     return (
         <div>
             <div class="footer">
-                <div class="second">
-                    <div class="main-logo1"></div>
-                    <div class="main-logo2"></div>
+                <div class="section-1">
+                    <div class="logo1"></div>
+                    <div class="logo2"></div>
                 </div>
-                <div class="feature">
-                    <h3>Features</h3>
-                    <div class="content">
-                        <div class="home">
-                            <a href="https://enactusadgitm.in/">Home</a>
-                        </div>
-                        <div class="about">
-                            <a href="#">About</a>
-                        </div>
-                        <div class="store">
-                            <a href="https://darpan.enactusadgitm.in/">Store</a>
-                        </div>
-                        <div class="projects">
-                            <a href="https://enactusadgitm.in/projects">
-                                Projects
-                            </a>
-                        </div>
-                        <div class="Team">
-                            <a href="#">Team</a>
-                        </div>
-                        <div class="events">
-                            <a href="#">Events</a>
-                        </div>
+                <div class="section-2">
+                    <div class="Features">Features</div>
+                    <div class="S1">
+                        <a href="https://enactusadgitm.in/">Home</a>
+                    </div>
+                    <div class="S2">
+                        <a href="https://enactusadgitm.in/#contact-form">
+                            About
+                        </a>
+                    </div>
+                    <div class="S3">
+                        <a href="https://darpan.enactusadgitm.in/">Store</a>
+                    </div>
+                    <div class="S4">
+                        <a href="https://enactusadgitm.in/projects">Projects</a>
+                    </div>
+                    <div class="S5">
+                        <a href="">Team</a>
+                    </div>
+                    <div class="S6">
+                        <a href="">Events</a>
                     </div>
                 </div>
-                <div class="first">
-                    <div class="heading">Connect With Us</div>
-                    <div class="socials">
-                        <a
-                            href="https://www.linkedin.com/company/enactus-adgitm"
-                            class="firstlogo"
-                        ></a>
-                        <a
-                            href="https://www.instagram.com/enactus.adgitm/"
-                            class="secondlogo"
-                        ></a>
-                        <a
-                            href="https://twitter.com/adgitmenactus"
-                            class="thirdlogo"
-                        ></a>
-                    </div>
-                    <div class="email">
-                        <div class="c1">enactusadgitm@gmail.com</div>
-                    </div>
-                    <div class="address">
-                        <div class="c2">
-                            FC-26, Panduk Shila Marg, Zero Pusta Rd, Shastri
-                            Park, Shahdara, New Delhi, Delhi-110053
+                <div class="section3">
+                    <div class="main">Connect With Us</div>
+                    <div class="Socials">
+                        <div class="insta">
+                            <a href="https://www.instagram.com/enactus.adgitm/"></a>
                         </div>
+                        <div class="linkdn">
+                            <a href="https://www.linkedin.com/company/enactus-adgitm"></a>
+                        </div>
+                        <div class="twitter">
+                            <a href="https://twitter.com/adgitmenactus"></a>
+                        </div>
+                    </div>
+                    <div class="mail">enactusadgitm@gmail.com</div>
+                    <div class="Address">
+                        FC-26,Panduk Shila Marg, Zero Pusta
+                        <br />
+                        Rd, Shashtri Park, Shahdara, New <br />
+                        Delhi, Delhi-110053
                     </div>
                 </div>
             </div>
             <div class="copyright">
-                <div class="l1">Enactus ADGITM © 2024</div>
-                <div class="l2">All Rights Reserved</div>
+                <div class="first">Enactus ADGITM © 2024</div>
+                <div class="Second">All Rights Reserved</div>
             </div>
         </div>
     )
