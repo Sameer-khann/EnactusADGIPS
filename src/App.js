57fa684053c35b0acd1bd7f8e6e2337723b1c1c5
Sameer Khan <sameerkhann241@gmail.com>
2024-03-14 01:05:04 +0530

    Team Page changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
 import LandingPage from '../src/Pages/LandingPage.jsx';
 import AboutPage from '../src/Pages/AboutPage.js';
 import EventPage from '../src/Pages/EventPage.js';
-import TeamPage from '../src/Pages/TeamPage.js';
+import TeamPage from '../src/Pages/TeamPage.jsx';
 import ProjectPage from '../src/Pages/ProjectPage.js';
 import Navbar from '../src/Components/Navbar.js';
 import Footer from '../src/Components/Footer.js';
--- src/Pages/TeamPage.jsx
+++ src/Pages/TeamPage.jsx
@@ -3,28 +3,50 @@ import img1 from '../img/Wallpaper.png'
 import img2 from '../img/wallpaperflare.com_wallpaper.jpg'
 import React, { useState, useRef } from 'react';
 
-export default function TeamPage(slides) {
+export default function TeamPage() {
 
-  const [slideIndex, setSlideIndex] = useState(0);
-  const slideRef = useRef(null);
-  const slideWidth = slideRef.current ? slideRef.current.offsetWidth : 0;
-
-  const prevSlide = () => {
-    setSlideIndex((prevIndex) =>
-      prevIndex > 0 ? prevIndex - 1 : slides.length - 1
-    );
-  };
-
-  const nextSlide = () => {
-    setSlideIndex((prevIndex) =>
-      prevIndex < slides.length - 1 ? prevIndex + 1 : 0
-    );
-  };
-
-  const slideStyle = {
-    transform: `translateX(-${slideIndex * slideWidth}px)`,
-    transition: 'transform 0.5s ease-in-out',
-  };
+  var right = 0;
+  var maxMargin;
+  var jumpMargin = 150;
+  
+  function setWidth(){
+    var boxwidth = document.querySelector(".item-box").offsetWidth;
+    var displaywidth = document.querySelector(".slider-container").offsetWidth;
+    var displayheight = document.querySelector(".slider-container").offsetHeight;
+    var children = document.querySelectorAll(".slides > .item-box").length;
+    var outerboxwidth = children * boxwidth + (children*10);
+    document.querySelector(".slides").style.width = outerboxwidth+"px";
+    document.querySelectorAll("button")[0].style.height = displayheight+"px";
+    document.querySelectorAll("button")[1].style.height = displayheight+"px";
+    maxMargin = outerboxwidth - displaywidth;
+  }
+  
+  function slideLeft(event){
+    var rowcont = document.querySelector(".slides");
+    if(right <= -maxMargin){
+      event.preventDefault();
+    }
+    else{
+      right -= jumpMargin;
+    }
+     rowcont.style.marginLeft = right+"px";
+  }
+  
+  function slideRight(event){
+    var rowcont = document.querySelector(".slides");
+    if(right==0){
+      event.preventDefault();
+    }
+    else if(right >= maxMargin){
+      event.preventDefault();
+    }
+    else{
+       right += jumpMargin;
+    }
+    rowcont.style.marginLeft = right+"px";
+  }
+  
+  window.onload=setWidth;
 
   return (
     <>
@@ -32,16 +54,17 @@ export default function TeamPage(slides) {
       <div>
       <div className="team">
         <h1>We Are Enactus</h1>
-        <div className="main_photo">
+        <div className='main_pic_frame'>
+          <div className="main_photo">
           <img src={img1}/>
           <img src={img2}/>
           <img src={img1}/>
           <img src={img2}/>
-
+          </div>
         </div>
         <div className="leads grid">
           <span>
-            <img src={img2} height="250px" width="250px" className="main" />
+            <img src={img2}  className="main" />
             <h2>president</h2>
             <h3>Vibhor jain</h3>
             <div className='info'>this to inform you that you can type any kind of text here</div>
@@ -62,9 +85,15 @@ export default function TeamPage(slides) {
       </div>
       <hr></hr>
       <div>
-        <h1>The council</h1>
+        <h1>The Council</h1>
       </div>
       <div className="council grid">
+        <span>
+          <img src={img2} height="250px" width="250px" className="main" />
+          <h2>president</h2>
+          <h3>Vibhor jain</h3>
+          <div className='info'>this to inform you that you can type any kind of text here</div>
+        </span>
         <span>
           <img src={img2} height="250px" width="250px" className="main" />
           <h2>president</h2>
@@ -89,8 +118,9 @@ export default function TeamPage(slides) {
         <h1>Here are our diligent department heads</h1>
       </div>
       <div className="slider-container">
+        <button type="button"  onClick={slideRight}>❮</button>
         <div className="slides">
-          <div className="slide">
+          <div className="item-box">
             <span>
               <img src={img2} height="200px" width="200px" className="main" /> 
               <h2>president</h2>
@@ -98,7 +128,7 @@ export default function TeamPage(slides) {
               <h5>lorem ipsum</h5>
             </span>
           </div>
-          <div className="slide">
+          <div className="item-box">
             <span>
               <img src={img2} height="200px" width="200px" className="main" /> 
               <h2>president</h2>
@@ -106,7 +136,7 @@ export default function TeamPage(slides) {
               <h5>lorem ipsum</h5>
             </span>
           </div>
-          <div className="slide">
+          <div className="item-box">
             <span>
               <img src={img2} height="200px" width="200px" className="main" /> 
               <h2>president</h2>
@@ -114,7 +144,7 @@ export default function TeamPage(slides) {
               <h5>lorem ipsum</h5>
             </span>
           </div>
-          <div className="slide">
+          <div className="item-box">
             <span>
               <img src={img2} height="200px" width="200px" className="main" /> 
               <h2>president</h2>
@@ -122,7 +152,7 @@ export default function TeamPage(slides) {
               <h5>lorem ipsum</h5>
             </span>
           </div>
-          <div className="slide">
+          <div className="item-box">
             <span>
               <img src={img2} height="200px" width="200px" className="main" /> 
               <h2>president</h2>
@@ -130,7 +160,7 @@ export default function TeamPage(slides) {
               <h5>lorem ipsum</h5>
             </span>
           </div>
-          <div className="slide">
+          <div className="item-box">
             <span>
               <img src={img2} height="200px" width="200px" className="main" /> 
               <h2>president</h2>
@@ -138,7 +168,7 @@ export default function TeamPage(slides) {
               <h5>lorem ipsum</h5>
             </span>
           </div>
-          <div className="slide">
+          <div className="item-box">
             <span>
               <img src={img2} height="200px" width="200px" className="main" /> 
               <h2>president</h2>
@@ -146,7 +176,7 @@ export default function TeamPage(slides) {
               <h5>lorem ipsum</h5>
             </span>
           </div>
-          <div className="slide">
+          <div className="item-box">
             <span>
               <img src={img2} height="200px" width="200px" className="main" /> 
               <h2>president</h2>
@@ -155,15 +185,14 @@ export default function TeamPage(slides) {
             </span>
           </div>
         </div>
-        <div className="prev" onClick={prevSlide}>&#10094;</div>
-        <div className="next" onClick={nextSlide}>&#10095;</div>
+        <button type="button" onClick={slideLeft}>❯</button>
       </div>
       <hr></hr>
       <div>
         <h1>Here are the previous council members</h1>
       </div>
       <div className="previous">
-      <h1>enactus 7.0</h1>
+      <h1>Enactus 7.0</h1>
         <div className="seven grid"> 
           <span>
             <img src={img2} height="250px" width="250px" className="main"/> 
@@ -171,6 +200,12 @@ export default function TeamPage(slides) {
             <h3>Vibhor jain</h3>
             <div className='info'>this to inform you that you can type any kind of text here</div>
           </span>
+          <span>
+          <img src={img2} height="250px" width="250px" className="main" />
+          <h2>president</h2>
+          <h3>Vibhor jain</h3>
+          <div className='info'>this to inform you that you can type any kind of text here</div>
+        </span>
           <span>
             <img src={img2} height="250px" width="250px" className="main"/>
             <h2>vice president</h2>
@@ -187,7 +222,7 @@ export default function TeamPage(slides) {
       </div>
       <hr></hr>
       <div>
-        <h1>enactus 6.0</h1>
+        <h1>Enactus 6.0</h1>
         <div className="six grid"> 
         <span>
             <img src={img2} height="250px" width="250px" className="main"/> 
@@ -207,11 +242,17 @@ export default function TeamPage(slides) {
             <h3>divyansh</h3>
             <div className='info'>this to inform you that you can type any kind of text here</div>
           </span>
+          <span>
+          <img src={img2} height="250px" width="250px" className="main" />
+          <h2>president</h2>
+          <h3>Vibhor jain</h3>
+          <div className='info'>this to inform you that you can type any kind of text here</div>
+        </span>
         </div>
       </div>
       <hr></hr>
       <div>
-        <h1>enactus 5.0</h1>
+        <h1>Enactus 5.0</h1>
         <div className="five grid"> 
         <span>
             <img src={img2} height="250px" width="250px" className="main"/> 
@@ -231,11 +272,17 @@ export default function TeamPage(slides) {
             <h3>divyansh</h3>
             <div className='info'>this to inform you that you can type any kind of text here</div>
           </span>
+          <span>
+          <img src={img2} height="250px" width="250px" className="main" />
+          <h2>president</h2>
+          <h3>Vibhor jain</h3>
+          <div className='info'>this to inform you that you can type any kind of text here</div>
+        </span>
         </div>
       </div>
       <hr></hr>
       <div>
-        <h1>enactus 4.0</h1>
+        <h1>Enactus 4.0</h1>
         <div className="four grid"> 
         <span>
             <img src={img2} height="250px" width="250px" className="main"/> 
@@ -255,11 +302,17 @@ export default function TeamPage(slides) {
             <h3>divyansh</h3>
             <div className='info'>this to inform you that you can type any kind of text here</div>
           </span>
+          <span>
+          <img src={img2} height="250px" width="250px" className="main" />
+          <h2>president</h2>
+          <h3>Vibhor jain</h3>
+          <div className='info'>this to inform you that you can type any kind of text here</div>
+        </span>
         </div>
       </div>
       <hr></hr>
       <div>
-        <h1>enactus 3.0</h1>
+        <h1>Enactus 3.0</h1>
         <div className="three grid"> 
         <span>
             <img src={img2} height="250px" width="250px" className="main"/> 
@@ -279,11 +332,17 @@ export default function TeamPage(slides) {
             <h3>divyansh</h3>
             <div className='info'>this to inform you that you can type any kind of text here</div>
           </span>
+          <span>
+          <img src={img2} height="250px" width="250px" className="main" />
+          <h2>president</h2>
+          <h3>Vibhor jain</h3>
+          <div className='info'>this to inform you that you can type any kind of text here</div>
+        </span>
         </div>
       </div>
       <hr></hr>
       <div>
-        <h1>enactus 2.0</h1>
+        <h1>Enactus 2.0</h1>
         <div className="two grid"> 
         <span>
             <img src={img2} height="250px" width="250px" className="main"/> 
@@ -303,11 +362,17 @@ export default function TeamPage(slides) {
             <h3>divyansh</h3>
             <div className='info'>this to inform you that you can type any kind of text here</div>
           </span>
+          <span>
+          <img src={img2} height="250px" width="250px" className="main" />
+          <h2>president</h2>
+          <h3>Vibhor jain</h3>
+          <div className='info'>this to inform you that you can type any kind of text here</div>
+        </span>
         </div>
       </div>
       <hr></hr>
       <div>
-        <h1>enactus 1.0</h1>
+        <h1>Enactus 1.0</h1>
         <div className="one grid"> 
         <span>
             <img src={img2} height="250px" width="250px" className="main"/> 
@@ -327,6 +392,12 @@ export default function TeamPage(slides) {
             <h3>divyansh</h3>
             <div className='info'>this to inform you that you can type any kind of text here</div>
           </span>
+          <span>
+          <img src={img2} height="250px" width="250px" className="main" />
+          <h2>president</h2>
+          <h3>Vibhor jain</h3>
+          <div className='info'>this to inform you that you can type any kind of text here</div>
+        </span>
         </div>
       </div>
     </div>
