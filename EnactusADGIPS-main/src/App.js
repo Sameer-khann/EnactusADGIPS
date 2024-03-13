3d2919b7e0c6ebd69245f4972b9cd4c2a8a7131f
Starkey <tanishqgupta013@gmail.com>
2024-03-13 11:49:39 +0530

    teampage fixed

--- EnactusADGIPS-main/src/App.js
+++ EnactusADGIPS-main/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
 import LandingPage from '../src/Pages/LandingPage.js';
 import AboutPage from '../src/Pages/AboutPage.js';
 import EventPage from '../src/Pages/EventPage.js';
-import TeamPage from '../src/Pages/TeamPage.js';
+import TeamPage from '../src/Pages/TeamPage.jsx';
 import ProjectPage from '../src/Pages/ProjectPage.js';
 import Navbar from '../src/Components/Navbar.js';
 import Footer from '../src/Components/Footer.js';
@@ -19,9 +19,9 @@ function App() {
         <Routes>
           {/* <Route path="/" element={<LandingPage/>} />
           <Route path="/AboutPage" element={<AboutPage/>} />
-          <Route path="/EventPage" element={<EventPage />} />
-          <Route path="/TeamPage" element={<TeamPage />} /> */}
-          <Route path="/" element={<ProjectPage/>} />
+          <Route path="/EventPage" element={<EventPage />} /> */}
+          <Route path="/" element={<TeamPage />} />
+          <Route path="/ProjectPage" element={<ProjectPage />} />
         </Routes>
         <Footer/>
       </Router>
